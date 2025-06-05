76a4d9d81dcf78cd10d56013c3d61a71bb3bcde9
mio99mk <miodragstosic99@gmail.com>
2025-06-06 00:26:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+git init
+git remote add origin https://github.com/mio99mk/msio_frontend.git
+touch requirements.txt
+echo "streamlit\nrequests" > requirements.txt
+git add .
+git commit -m "frontend init"
+git push -u origin main
